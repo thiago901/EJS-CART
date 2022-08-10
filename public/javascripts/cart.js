5e360b49417f92de6888c1e7c55f995f5764350f
= <=>
2022-08-10 11:40:12 -0300

    init

---
+++ public/javascripts/cart.js
@@ -0,0 +1,75 @@
+const KEY_STORAGE = '@shoes:cart';
+
+
+function getCookie(name) {
+  var cookies = document.cookie;
+  var prefix = name + "=";
+  var begin = cookies.indexOf("; " + prefix);
+
+  if (begin == -1) {
+
+      begin = cookies.indexOf(prefix);
+       
+      if (begin != 0) {
+          return null;
+      }
+
+  } else {
+      begin += 2;
+  }
+
+  var end = cookies.indexOf(";", begin);
+   
+  if (end == -1) {
+      end = cookies.length;                        
+  }
+
+  return cookies.substring(begin + prefix.length, end);
+}
+function getCart (){
+  let cart = getCookie()
+  if(!cart){
+    cart='[]';
+  }
+
+  cart = JSON.parse(cart);
+
+  return cart
+}
+function save(cart){
+  document.cookie = `${KEY_STORAGE}=${JSON.stringify(cart)}`
+}
+
+function addToCart(product){
+
+
+  const cart = getCart();
+
+  const hasItemIndex = cart.findIndex(item=>item.id ===product.id);
+
+  if(hasItemIndex>=0){
+    cart[hasItemIndex].amount = cart[hasItemIndex].amount+1;
+    console.log(product);
+  }else{
+    cart.push({
+      ...product,
+      amount:1
+    });
+  }
+
+  save(cart)
+}
+
+function removeItem(id){
+  const cart = getCart();
+  const hasItemIndex = cart.findIndex(item=>item.id ===product.id);
+  if(hasItemIndex>=0){
+    
+    if(cart[hasItemIndex].amount>0){
+      cart[hasItemIndex].amount=cart[hasItemIndex].amount -1;
+    }else{
+      cart.split(1,hasItemIndex);
+    }
+    save(cart)
+  }
+}
---
+++ routes/cart.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  const KEY_STORAGE = '@shoes:cart';
+  let cart = req.cookies[KEY_STORAGE];
+  if(!cart){
+    cart='[]';
+  }
+  cart = JSON.parse(cart);
+  const cartFormatted = cart.map(item=>({
+    ...item,
+    subTotal:item.price * item.amount
+  }))
+  const total = cartFormatted.reduce((acc,current)=>acc+Number(current.subTotal),0)
+  res.render('cart',{
+    cart,
+    total
+  });
+});
+
+module.exports = router;
